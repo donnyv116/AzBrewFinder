df6affb2d5cf0aa76c2fafc4cbef424b892272d7
Andy Sargent <Andyjohnsargent@gmail.com>
2018-03-02 23:03:59 -0700

    fixed the damn user.js sequelize issues

--- models/user.js
+++ models/user.js
@@ -1,29 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('Users', {
+const Users = sequelize.define('Users', { 
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-
-        Users.belongsToMany(models.breweries, {through: 'BreweryLikes'});
-        // Users.belongsToMany(models.Cities, {through: 'CityLikes'});
-        
-        
-      }
-    },
-    instanceMethods: {
-      generateHash: function(password) {
-        return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-      },
-      validPassword: function(password) {
-        return bcrypt.compareSync(password, this.password);
-      },
-    }
   });
+
+
+// Class Method
+Users.associate = function (models) {
+    Users.belongsToMany(models.Breweries, {through: 'BreweryLikes'});
+};
+
+// Instance Method
+Users.prototype.generateHash = function (password) {return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);},
+Users.prototype.validPassword = function (password) {return bcrypt.compareSync(password, this.password);},
 
   Users.hook('beforeCreate', function(user, options) {
     user.password = user.generateHash(user.password);
